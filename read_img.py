f4f8385877adcb53ad3bc7215f58214ca7df3b03
LiuPhyMan <liu.jinbao@outlook.com>
2018-07-29 19:46:41 +0800

    Init

--- read_img.py
+++ read_img.py
@@ -10,9 +10,8 @@ Created on 15:05 2018/7/28
 """
 from matplotlib import pyplot as plt
 import cv2
-# cv2.namedWindow("output", cv2.WINDOW_NORMAL)        # Create window with freedom of dimensions
-im = cv2.imread("D:\Desktop\DSC_0759.JPG")                        # Read image
-imS = cv2.resize(im, (960, 540))                    # Resize image
+
+im = cv2.imread("D:\Desktop\DSC_0759.JPG")  # Read image
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.imshow(im)
@@ -20,6 +19,8 @@ ax.imshow(im)
 total_num = 3
 out_x = []
 out_y = []
+
+
 def click_callback(event):
     out_x.append(event.xdata)
     out_y.append(event.ydata)
@@ -29,6 +30,5 @@ def click_callback(event):
         print(out_x)
         print(out_y)
 
+
 cid = fig.canvas.mpl_connect('button_press_event', click_callback)
-# cv2.imshow("output", imS)                            # Show image
-# cv2.waitKey(0)
